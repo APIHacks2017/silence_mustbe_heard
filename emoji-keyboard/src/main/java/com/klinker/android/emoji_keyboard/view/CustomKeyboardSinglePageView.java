270cff8e97aff4300135725358bedaa4e19e21c1
Nirmalkumar Sathiamurthi <nirmal@Nirmalkumars-MacBook-Air.local>
2017-06-17 20:05:10 +0530

    Added GPS sharing feature

--- emoji-keyboard/src/main/java/com/klinker/android/emoji_keyboard/view/CustomKeyboardSinglePageView.java
+++ emoji-keyboard/src/main/java/com/klinker/android/emoji_keyboard/view/CustomKeyboardSinglePageView.java
@@ -1,9 +1,18 @@
 package com.klinker.android.emoji_keyboard.view;
 
+import android.Manifest;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
@@ -32,7 +41,10 @@ import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.nostra13.universalimageloader.core.assist.ImageScaleType;
 import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
 
+import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Locale;
 
 import okhttp3.OkHttpClient;
 import okhttp3.internal.Util;
@@ -48,12 +60,14 @@ import static com.klinker.android.emoji_keyboard.network.Endpoints.SERVER_BASEUR
 public class CustomKeyboardSinglePageView {
 
     private final Gson gson;
+    private final LocationManager locationManager;
+    private final LocationListener locationListener;
     private Context context;
     private BaseAdapter adapter;
     private Endpoints mAPI;
     private DisplayImageOptions imOptions;
 
-    public CustomKeyboardSinglePageView(Context context, BaseAdapter adapter) {
+    public CustomKeyboardSinglePageView(final Context context, BaseAdapter adapter) {
         this.context = context;
         this.adapter = adapter;
         gson = new GsonBuilder().
@@ -75,9 +89,60 @@ public class CustomKeyboardSinglePageView {
                 .build();
         this.mAPI = retrofit.create(Endpoints.class);
 
+        locationManager = (LocationManager)
+                context.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location loc) {
+                String longitude = "Longitude: " + loc.getLongitude();
+                String latitude = "Latitude: " + loc.getLatitude();
 
+        /*------- To get city name from coordinates -------- */
+                String cityName = null;
+                Geocoder gcd = new Geocoder(context, Locale.getDefault());
+                List<Address> addresses;
+                try {
+                    addresses = gcd.getFromLocation(loc.getLatitude(),
+                            loc.getLongitude(), 1);
+                    if (addresses.size() > 0) {
+                        System.out.println(addresses.get(0).getLocality());
+                        cityName = addresses.get(0).getLocality();
+                    }
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Utility.saveKeyValueInPref(context,"gps",latitude+","+longitude);
+                Log.d("ui","GPS updated");
+            }
 
-        Log.d("ui","init page view");
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
+            Log.d("ui","GPS called");
+        }
+
     }
 
 
@@ -93,9 +158,19 @@ public class CustomKeyboardSinglePageView {
             @Override
             public void onClick(View v) {
                 EmojiKeyboardService mSrv = (EmojiKeyboardService) context;
-                mSrv.sendText("Hey I m in at https://goo.gl/maps/gb9QSggKYNw");
+                if (Utility.getValueInPref(context,"gps")!=null)
+                    mSrv.sendText("Hey I m in at https://goo.gl/maps/gb9QSggKYNw  "+Utility.getValueInPref(context,"gps"));
+                else
+                    mSrv.sendText("Hey I m in at https://goo.gl/maps/gb9QSggKYNw ");
             }
         });
+
+        try {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
+            Log.d("ui", "GPS called");
+        }catch(Exception e){
+
+        }
 
         customPage.findViewById(R.id.shareAddress).setOnClickListener(new View.OnClickListener() {
             @Override
